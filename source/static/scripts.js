ab388569a4aae840e9b00d551accac87edbe6e16
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-04-23 15:31:16 -0400

    [TO REVERT] Add commented code to autoscroll

--- source/static/scripts.js
+++ source/static/scripts.js
@@ -13,6 +13,9 @@ _paq.push(['enableLinkTracking']);
 })();
 
 $(document).ready(function() {
+    // var activeMenuItem = $('.current').slice(-1)[0];
+    // activeMenuItem.scrollIntoView();
+
     $('a[href^="https://"], a[href^="http://"]').attr("target", "_blank"); // Make all external links open in a new Window
 
     // Internal docs containing an anchor are wrongly transpiled to an absolute link starting by '/'. This causes dead links when we serve under openfisca.org/doc. The following manually fix these links in JS. This could be avoided if we served on doc.openfisca.org
